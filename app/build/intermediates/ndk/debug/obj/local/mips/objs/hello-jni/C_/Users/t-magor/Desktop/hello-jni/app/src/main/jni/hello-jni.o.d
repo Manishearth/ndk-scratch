6b4df118200a20e0643db24c651fe29c7b367a26
Manish Goregaokar <manishsmail@gmail.com>
2015-05-21 15:17:53 +0530

    init

---
+++ app/build/intermediates/ndk/debug/obj/local/mips/objs/hello-jni/C_/Users/t-magor/Desktop/hello-jni/app/src/main/jni/hello-jni.o.d
@@ -0,0 +1,23 @@
+C:\Users\t-magor\Desktop\hello-jni\app\build\intermediates\ndk\debug\obj/local/mips/objs/hello-jni/C_\Users\t-magor\Desktop\hello-jni\app\src\main\jni\hello-jni.o: \
+ C:\Users\t-magor\Desktop\hello-jni\app\src\main\jni\hello-jni.c \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/string.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/sys/cdefs.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/sys/cdefs_elf.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/android/api-level.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/malloc.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/xlocale.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/jni.h
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/string.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/sys/cdefs.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/sys/cdefs_elf.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/android/api-level.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/malloc.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/xlocale.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-mips/usr/include/jni.h:
---
+++ app/build/intermediates/ndk/debug/obj/local/x86/objs/hello-jni/C_/Users/t-magor/Desktop/hello-jni/app/src/main/jni/hello-jni.o.d
@@ -0,0 +1,23 @@
+C:\Users\t-magor\Desktop\hello-jni\app\build\intermediates\ndk\debug\obj/local/x86/objs/hello-jni/C_\Users\t-magor\Desktop\hello-jni\app\src\main\jni\hello-jni.o: \
+ C:\Users\t-magor\Desktop\hello-jni\app\src\main\jni\hello-jni.c \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/string.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs_elf.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/android/api-level.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/malloc.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/xlocale.h \
+ C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/jni.h
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/string.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/sys/cdefs_elf.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/android/api-level.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/malloc.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/xlocale.h:
+
+C:/Users/t-magor/Documents/android-ndk-r10e/platforms/android-21/arch-x86/usr/include/jni.h:
