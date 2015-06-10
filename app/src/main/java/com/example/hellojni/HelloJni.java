c50daaee321eac9b5bcf1f0031e7f92fa4d41dca
Manish Goregaokar <manishsmail@gmail.com>
2015-06-10 17:49:22 +0530

    context creation works

--- app/src/main/java/com/example/hellojni/HelloJni.java
+++ app/src/main/java/com/example/hellojni/HelloJni.java
@@ -16,33 +16,43 @@
 package com.example.hellojni;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ScaleDrawable;
+import android.graphics.SurfaceTexture;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
-import android.view.Gravity;
 import android.view.MotionEvent;
-import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
-import android.widget.TextView;
-import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 
-import java.util.Random;
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+import javax.microedition.khronos.opengles.GL11;
 
 
-public class HelloJni extends Activity implements View.OnTouchListener
+@SuppressLint("NewApi")
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class HelloJni extends Activity implements TextureView.SurfaceTextureListener, View.OnTouchListener
 {
-    BitmapDrawable bd1;
-    BitmapDrawable bd2;
-    SurfaceView view;
+
     int height, width;
-    DualBitmap dual1;
-    DualBitmap dual2;
+    TextureView view;
+    int[] textureHandle;
+    SurfaceTexture st;
+    private GLSurfaceView mGLView;
 
     /** Called when the activity is first created. */
     @SuppressWarnings("deprecation")
@@ -50,40 +60,26 @@ public class HelloJni extends Activity implements View.OnTouchListener
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
 
         super.onCreate(savedInstanceState);
-
-        /* Create a TextView and set its content.
-         * the text is retrieved by calling a native
-         * function.
-         */
-        /*
-        TextView  tv = new TextView(this);
-        tv.setText( stringFromJNI() );*/
-
-        view = new SurfaceView(this);
-
+          textureHandle = new int[1];
+        GLES20.glGenTextures(1, textureHandle, 0);
+        view = new TextureView(this);
+        view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         height = size.y;
         width = size.x;
-        Log.i("Manish h", height + " Manish");
-        Log.i("Manish w", width + " Manish");
-        height = height / 2;
-        width = width / 2;
-        dual1 = new DualBitmap(width, height);
-        dual2 = new DualBitmap(width, height);
-        bd1 = new BitmapDrawable(dual1.getBitmap());
-        bd2 = new BitmapDrawable(dual2.getBitmap());
-        view.setBackground(bd1);
-        bd1.setGravity(Gravity.CENTER);
-        bd2.setGravity(Gravity.CENTER);
-        view.setOnTouchListener(this);
-        current = 1;
-        //dual1.fillC();
-        //dual2.fillC();
+        view.setSurfaceTextureListener(this);
+
+        view.setLayoutParams(new ViewGroup.LayoutParams(width, height));
         setContentView(view);
+        view.setVisibility(View.VISIBLE);
+
     }
 
     /* A native method that is implemented by the
@@ -107,57 +103,157 @@ public class HelloJni extends Activity implements View.OnTouchListener
     @SuppressLint("NewApi")
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        long timeStart, timeEnd;
-        current = 1;
-        DualBitmap holder = new DualBitmap(width, height);
-        if (state) {
-
-
-            DualBitmap db = new DualBitmap(width, height);
-            timeStart = System.currentTimeMillis();
-            for (int i=0; i<1000;i++) {
-                db.fillC();
-            }
-            timeEnd = System.currentTimeMillis();
-            db.close();
-            Log.i("Profiling", "BAlloc:"+ (timeEnd - timeStart));
-        } else {
-            holder.UNSAFE_ALLOC(width, height);
-            timeStart = System.currentTimeMillis();
-            for (int i=0; i<1000;i++) {
-
-                holder.fillC();
-
-            }
-            timeEnd = System.currentTimeMillis();
-            Log.i("Profiling", "CAlloc:"+ (timeEnd - timeStart));
-            holder.UNSAFE_FREE();
-        }
-
-        state = !state;
-        view.invalidate();
+        view.setSurfaceTexture(st);
+       // st.updateTexImage();
         return false;
     }
 
-    @SuppressLint("NewApi")
-    void fillSwap() {
-        if (current == 1) {
-            dual2.fillC();
-            view.setBackground(bd2);
-        } else {
-            dual1.fillC();
-            view.setBackground(bd1);
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.i("Profiling", "reached");
+        mSurface = surface;
+        initGL();
+        textureHandle = new int[1];
+
+        GLES20.glGenTextures(1, textureHandle, 0);
+        checkGlError();
+        if (textureHandle[0] != 0) {
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            for (int i = 1; i< this.height -1; i++) {
+                for(int j = 1; j<this.width -1; j++) {
+                    bitmap.setPixel(j, i, Color.RED);
+                }
+            }
+            // Bind to the texture in OpenGL
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
+            checkGlError();
+
+            // Set filtering
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+            checkGlError();
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+            checkGlError();
+        }
+        @SuppressLint("Recycle") SurfaceTexture st = new SurfaceTexture(textureHandle[0]);
+
+    }
+    private static final int EGL_OPENGL_ES2_BIT = 4;
+    private static final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
+    private EGLDisplay mEglDisplay;
+    private EGLSurface mEglSurface;
+    private EGLContext mEglContext;
+    private SurfaceTexture mSurface;
+    private EGL10 mEgl;
+    private GL11 mGl;
+    private void initGL() {
+
+
+        mEgl = (EGL10) EGLContext.getEGL();
+
+        mEglDisplay = mEgl.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        if (mEglDisplay == EGL10.EGL_NO_DISPLAY) {
+            throw new RuntimeException("eglGetDisplay failed "
+                    + GLUtils.getEGLErrorString(mEgl.eglGetError()));
         }
 
-        current = 3 - current;
+        int[] version = new int[2];
+        if (!mEgl.eglInitialize(mEglDisplay, version)) {
+            throw new RuntimeException("eglInitialize failed "
+                    + GLUtils.getEGLErrorString(mEgl.eglGetError()));
+        }
+
+        int[] configsCount = new int[1];
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] configSpec = {
+                EGL10.EGL_RENDERABLE_TYPE,
+                EGL_OPENGL_ES2_BIT,
+                EGL10.EGL_RED_SIZE, 8,
+                EGL10.EGL_GREEN_SIZE, 8,
+                EGL10.EGL_BLUE_SIZE, 8,
+                EGL10.EGL_ALPHA_SIZE, 8,
+                EGL10.EGL_DEPTH_SIZE, 0,
+                EGL10.EGL_STENCIL_SIZE, 0,
+                EGL10.EGL_NONE
+        };
+
+        EGLConfig eglConfig = null;
+        if (!mEgl.eglChooseConfig(mEglDisplay, configSpec, configs, 1,
+                configsCount)) {
+            throw new IllegalArgumentException(
+                    "eglChooseConfig failed "
+                            + GLUtils.getEGLErrorString(mEgl
+                            .eglGetError()));
+        } else if (configsCount[0] > 0) {
+            eglConfig = configs[0];
+        }
+        if (eglConfig == null) {
+            throw new RuntimeException("eglConfig not initialized");
+        }
+        int i = Integer.parseInt("1");
+        int[] attrib_list = {
+                EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE
+        };
+        mEglContext = mEgl.eglCreateContext(mEglDisplay,
+                eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list);
+        checkEglError();
+        mEglSurface = mEgl.eglCreateWindowSurface(
+                mEglDisplay, eglConfig, mSurface, null);
+        checkEglError();
+        if (mEglSurface == null || mEglSurface == EGL10.EGL_NO_SURFACE) {
+            int error = mEgl.eglGetError();
+            if (error == EGL10.EGL_BAD_NATIVE_WINDOW) {
+                Log.e("egl",
+                        "eglCreateWindowSurface returned EGL10.EGL_BAD_NATIVE_WINDOW");
+                return;
+            }
+            throw new RuntimeException(
+                    "eglCreateWindowSurface failed "
+                            + GLUtils.getEGLErrorString(error));
+        }
+
+        if (!mEgl.eglMakeCurrent(mEglDisplay, mEglSurface,
+                mEglSurface, mEglContext)) {
+            throw new RuntimeException("eglMakeCurrent failed "
+                    + GLUtils.getEGLErrorString(mEgl.eglGetError()));
+        }
+        checkEglError();
+
+        mGl = (GL11) mEglContext.getGL();
+        checkEglError();
+/*
+        mProgram = buildProgram(vertexShaderSource,
+                fragmentShaderSource);
+        */
     }
-    void fillCopy() {
-        if (current == 1) {
-            dual2.fillC();
-            dual2.copyTo(dual1);
-        } else {
-            dual1.fillC();
-            dual1.copyTo(dual2);
+
+    private void checkGlError() {
+        final int error = mGl.glGetError();
+        if (error != GL11.GL_NO_ERROR) {
+            Log.e("Egl", "GL error = 0x" + Integer.toHexString(error));
         }
     }
+    private void checkEglError() {
+        final int error = mEgl.eglGetError();
+        if (error != EGL10.EGL_SUCCESS) {
+            Log.e("Egl", "EGL error = 0x" + Integer.toHexString(error));
+        }
+    }
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+
+
 }
